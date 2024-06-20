5eedd2bbddb110e1172bb602f3933f621d35d852
Denis Cipurko <63480747+dcipurko@users.noreply.github.com>
2024-06-20 18:22:09 -0500

    Update __init__.py

--- arrayseq/plotting/__init__.py
+++ arrayseq/plotting/__init__.py
@@ -5,3 +5,5 @@ from .Plotting_2D import plot_multi_gene
 from .Plotting_3D import plot_meta_3d
 from .Plotting_3D import plot_gene_3d
 from .Plotting_3D import plot_HE_3d
+
+__all__ = ['plot_meta', 'plot_gene', 'plot_multi_gene', 'plot_meta_3d', 'plot_gene_3d', 'plot_HE_3d']
