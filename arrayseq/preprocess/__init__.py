d279b2487dab5f562f35df23b7b68032ae543035
Denis Cipurko <63480747+dcipurko@users.noreply.github.com>
2024-06-20 18:29:05 -0500

    Update __init__.py

--- arrayseq/preprocess/__init__.py
+++ arrayseq/preprocess/__init__.py
@@ -5,3 +5,15 @@ from .Manual_align import maunal_align
 
 from .Multi_Tissue_and_3D import seperate_tissues
 from .Multi_Tissue_and_3D import align_3D
+
+from .Image_processing import preprocess_histology_image
+from .Image_processing import detect_tissue
+
+
+__all__ = ['aulto_align', 
+           'generate_manual_ST_image', 
+           'maunal_align', 
+           'seperate_tissues', 
+           'align_3D',
+           'preprocess_histology_image',
+           'detect_tissue']
